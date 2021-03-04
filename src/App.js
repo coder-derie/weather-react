9f7e8ae2b1ebd73b1005c934f55b6428929907aa
coder-derie <77130913+coder-derie@users.noreply.github.com>
2021-03-04 12:25:27 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <Weather />
+        <p>Great!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(){
+    const [city, setCity] = useState("");
+    
+    
+    function handleSubmit(event){
+        event.preventDefault();
+        let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4aebdc826d2f6fe4955b6c3fa809665d&units=metric`
+        axios.get(apiUrl).then(showWeather);
+
+    }
+    function updateCity(event){
+        setCity(event.target.value);
+    }
+    function showWeather(response){
+        alert(`Welcome to ${city}! It is ${response.data.main.temp} ℃`);
+    }
+    let form =(
+        <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Type a city" onChange={updateCity}/>
+                <input type="submit" value="Search" />
+            </form>
+            )
+    
+        return(
+        <div className="Weather">
+            <h2>This is my first time in VS code</h2>
+            {form}
+            <Loader
+        type="BallTriangle"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+            </div>
+    )
+    
+    
+}
