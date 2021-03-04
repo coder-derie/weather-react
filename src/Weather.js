580c672ec9c76ae0adbbf1adc43221d135e99d77
coder-derie <77130913+coder-derie@users.noreply.github.com>
2021-03-04 14:18:35 -0500

    Edited the loader

--- src/Weather.js
+++ src/Weather.js
@@ -34,7 +34,7 @@ export default function Weather(){
         color="#00BFFF"
         height={100}
         width={100}
-        timeout={3000} //3 secs
+        timeout={3000} 
       />
             </div>
     )
